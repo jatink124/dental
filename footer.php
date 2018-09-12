fa2ff386f9535ce0022e6ce15dc3cecb8a9f6f88
jatink124 <kaushaljatin48@gmail.com>
2018-09-12 17:57:30 +0530

    test in new

--- footer.php
+++ footer.php
@@ -11,13 +11,11 @@ $('.slider').slider();
 <script src="js/myJs.js"></script>
 <script src="js/letterEffect.js"></script>
 <script src="js/switchtext.js"></script>
-<script src="https://cdn.jsdelivr.net/jquery.event.drag/2.2/jquery.event.drag.min.js"></script>
-<script src="http://cdnjs.cloudflare.com/ajax/libs/jquery-mousewheel/3.1.12/jquery.mousewheel.js"></script>
-<script src="http://cdnjs.cloudflare.com/ajax/libs/jquery-easing/1.3/jquery.easing.min.js"></script>
+
 <script src="js/jquery.cross2.js"></script>
 <script src="Custom.js"></script>
 <script src="js/new_image_slider.js"></script>
-<script src="js/toArrayFilter.js"></script>
+<script src="js/toArrayFil.js"></script>
 
 </body>
 </html>
